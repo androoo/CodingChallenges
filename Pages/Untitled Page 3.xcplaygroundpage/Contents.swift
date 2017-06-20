d5193cbd9eb0343825f8554c7d16370b906f3ab5
Andrew Gierke <andrew.gierke@gmail.com>
2017-06-20 16:13:57 -0600

    fizzbuzz redux

---
+++ Pages/Untitled Page 3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Fizz Buzz
+
+func fizzBuzz() {
+    
+    for i in 1...100 {
+        if i % 3 == 0 {
+            if i % 5 == 0 {
+                print("FizzBuzz")
+            } else {
+                print("Fizz")
+            }
+        } else if i % 5 == 0 {
+            print("Buzz")
+        } else {
+            print(i)
+        }
+    }
+    
+}
+
+fizzBuzz()
+
+func fizzBuzzb() {
+    
+    (1...100).forEach {
+        
+        print($0 % 3 == 0 ? $0 % 5 == 0 ? "Fizz Buz" : "Fizz" : $0 % 5 == 0 ? "Buzz" : "\($0)")
+        
+    }
+}
+
+//fizzBuzzb()
+
+//: [Next](@next)
