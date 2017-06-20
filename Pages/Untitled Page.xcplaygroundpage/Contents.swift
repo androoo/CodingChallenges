20e6f0de03c624632736b31d62d943e8ab1b8c29
Andrew Gierke <andrew.gierke@gmail.com>
2017-06-20 15:26:20 -0600

    13

---
+++ Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 13 - String Permutations 
+
+// Write a function athat prints all possible permutaitons of a given input string. 
+
+func stringLetterPermutations(string: String, current: String = "") {
+    let length = string.characters.count
+    let strArray = Array(string.characters)
+    
+    if (length == 0) {
+        print(current)
+        print("*****")
+    } else {
+        print(current)
+        for i in 0..<length {
+            let left = String(strArray[0..<i])
+            let right = String(strArray[i + 1..<length])
+            stringLetterPermutations(string: left, current: current + String(strArray[i]))
+        }
+    }
+}
+
+stringLetterPermutations(string: "andrew", current: "")
+
+
+//: [Next](@next)
