1c602c6886f5be8226268d99113983bbdad5532e
Andrew Gierke <andrew.gierke@gmail.com>
2017-06-20 15:42:08 -0600

    reversing words in a string but retaining order

---
+++ Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 14 - Reverse the word in a string 
+
+// Return a string with each of its words reversed but in the same order without using a loop.
+
+func reversEachString(input: String) -> String {
+    
+    let charSet = NSMutableCharacterSet.whitespacesAndNewlines
+    let parts = input.components(separatedBy: charSet)
+    
+    let reversed = parts.map{ String($0.characters.reversed()) }
+    
+    return reversed.joined(separator: " ")
+    
+}
+
+reversEachString(input: "I am in Utah")
+
+
+//: [Next](@next)
