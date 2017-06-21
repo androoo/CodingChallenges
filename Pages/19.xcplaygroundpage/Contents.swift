e1760b05408cc64fcf6759701191a2c827a5c649
Andrew Gierke <andrew.gierke@gmail.com>
2017-06-21 14:36:16 -0600

    swap numbers

---
+++ Pages/19.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 19 - Swap two numbers 
+
+// Swap two positive variable integers, a and b, without using a temporary variable 
+
+
+
+var a = 1
+var b = 2
+
+(a, b) = (b, a)
+
+//: [Next](@next)
